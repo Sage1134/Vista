4607a0dba54f2466905c05f0ee79b0e485da7f56
Kevin Wang <kayhne999@gmail.com>
2025-01-17 23:02:54 -0500

    expensive luma gen - use cheaper solution instead

--- bread_example.py
+++ bread_example.py
@@ -1,15 +1,3 @@
-from lumaai import LumaAI
-from dotenv import load_dotenv
-from os import getenv
-import requests
-import time
-
-print("Are you sure you want to run? You must run the whole file WITHOUT STOP, or you will be wasting $0.40.")
-user_input = input("Proceed? (Y/N) ")
-
-if user_input != "Y":
-    raise RuntimeError("User chose to stop.")
-
 HOW_TO_BAKE_BREAD="""How to make Bread:
 Proof the yeast: In a large bowl or stand mixer add the yeast, water and a pinch of the sugar or honey. Allow to rest for 5-10 minutes until foaming and bubbly. (This is called “proofing” the yeast, to make sure it is active. If it doesn’t foam, the yeast is no good, and you need to start over with fresh yeast).
 Prepare the dough: Add remaining sugar or honey, salt, oil, and 3 cups of flour. Mix to combine. Add another cup of flour and mix to combine. With the mixer running add more flour, ½ cup at a time, until the dough begins to pull away from the sides of the bowl.
@@ -33,39 +21,11 @@ A loaf of homemade white bread cooling on a wire rack.
 
 Storing: Once cool, store bread in an airtight container or bag for 2-3 days at room temperature, or up to 5 days in the refrigerator."""
 
-load_dotenv()
-LUMA_API_KEY = getenv('LUMA_KEY')
+HOW_QUESTION="How do I bake bread?"
 
-client = LumaAI(
-    auth_token=LUMA_API_KEY
-)
-
-# How to make bread?
-generation = client.generations.create(
-    prompt=HOW_TO_BAKE_BREAD, 
-    aspect_ratio="4:3", 
-    loop=False
-)
-
-
-start = time.time()
-completed = False
-while not completed:
-    generation = client.generations.get(id=generation.id)
-    if generation.state == 'completed':
-        end = time.time()
-        completed = True
-    elif generation.state == 'failed':
-        end = time.time()
-        raise RuntimeError(f"Generation failed: {generation.failure_reason}")
-    print("Generating video...")
-    time.sleep(3)
-    
-video_url = generation.assets.video
-    
-# get the video, once its done
-response = requests.get(video_url, stream=True)
-with open(f'./videos/{generation.id}.mp4', 'wb') as file:
-    file.write(response.content)
-print(f"File downloaded as ./videos/{generation.id}.mp4") # - $0.40 per run lol
-print(f"Took {end - start} seconds to generate that.")
+BREAD_STEPS = [
+    "Proof the yeast: In a large bowl or stand mixer add the yeast, water and a pinch of the sugar or honey. Allow to rest for 5-10 minutes until foaming and bubbly. (This is called “proofing” the yeast, to make sure it is active. If it doesn’t foam, the yeast is no good, and you need to start over with fresh yeast).",
+    "Prepare the dough: Add remaining sugar or honey, salt, oil, and 3 cups of flour. Mix to combine. Add another cup of flour and mix to combine. With the mixer running add more flour, ½ cup at a time, until the dough begins to pull away from the sides of the bowl.",
+    "First Rise: Grease a large bowl with oil or cooking spray and place the dough inside. Cover with a dish towel or plastic wrap and allow to rise in a warm place* until doubled in size (about 1 ½ hours).",
+    "Bake dough in oven at 350F."
+]
